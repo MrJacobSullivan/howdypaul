32eb504098b0181d4c27104c1dd6b59eb2917bd7
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-07-23 09:09:40 -0500

    back to normal

--- src/main.js
+++ src/main.js
@@ -1,10 +1,3 @@
 import '../styles/style.scss'
 
 const jacob = document.getElementById('jacob')
-
-const disguise = document.getElementById('disguise')
-
-disguise.addEventListener('click', () => {
-  disguise.remove()
-  jacob.src = '/img/HowdyPaulDisguise.gif'
-})
